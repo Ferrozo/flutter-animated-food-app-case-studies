8a8af75f3e5ac3a3aa5d3dcb56c35a4a2fd238d2
Ferrozo <ferrozo.c@gmail.com>
2022-12-09 00:53:59 +0100

    setting up the project

---
+++ android/app/src/main/kotlin/com/example/animated_food_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animated_food_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:animated_food_app/src/presentation/screens/app/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
